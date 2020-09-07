1379e165ed53685f01467878c1d128cfad4270a8
shira-d111 <67634067+shira-d111@users.noreply.github.com>
2020-09-07 15:03:21 +0300

    DB include join

--- db.py
+++ db.py
@@ -1,7 +1,6 @@
 import os
 from BTrees.OOBTree import OOBTree
 
-
 import db_api
 import csv
 from typing import Any, Dict, List, Type
@@ -27,6 +26,8 @@ def calculateOp(value1: Any, value2: Any, operator: str) -> bool:
         return value1 > value2
     if operator == "<":
         return value1 < value2
+    if operator == ">=":
+        return value1 >= value2
 
 
 def orderTheKey(field: List[db_api.DBField], key_field_name: str) -> None:
@@ -53,7 +54,7 @@ class DBTable(db_api.DBTable):
         self.key_field_name = key_field_name
         self.files.append(os.path.abspath(os.path.join("db_files", f'{name}1.csv')))
         writeToFile(self.files[0], 'w', [self.fields])
-        #self.index[key_field_name] = {}
+        # self.index[key_field_name] = {}
 
     def search(self, key: Any) -> (str, int):
         if self.key_field_name in self.index:
@@ -86,10 +87,10 @@ class DBTable(db_api.DBTable):
 
         writeToFile(filename, 'a', [[values.get(field.name) for field in self.fields]])
         for i in self.index:
-            self.index[i][values[i]] = filename,  self.counterRecords % 1500
+            self.index[i][values[i]] = filename, self.counterRecords % 1500
 
     def delete_record(self, key: Any) -> None:
-        file, index = self.search(key)
+        file, pos = self.search(key)
         if file is None:
             raise ValueError
         self.counterRecords -= 1
@@ -100,10 +101,19 @@ class DBTable(db_api.DBTable):
         if lastRecord[0][0] != str(key):
             for i in self.index:
                 indexOfField = [j for j, f in enumerate(self.fields) if f.name == i][0]
-                self.index[i].pop(int(csv_reader[index][indexOfField]))
+                type_ = self.fields[indexOfField].type
+                prev = self.index[i].pop(type_(csv_reader[pos][indexOfField]))
+                self.index[i][type_(lastRecord[0][indexOfField])] = prev
+
             csv_reader = readFromFile(file)
-            csv_reader[index] = lastRecord[0]
+            csv_reader[pos] = lastRecord[0]
+
             writeToFile(self.files[-1], 'w', csv_reader)
+        else:
+            for i in self.index:
+                indexOfField = [j for j, f in enumerate(self.fields) if f.name == i][0]
+                type_ = self.fields[indexOfField].type
+                self.index[i].pop(type_(csv_reader[pos][indexOfField]))
 
     def delete_records(self, criteria: List[db_api.SelectionCriteria]) -> None:
         for file in self.files:
@@ -149,7 +159,9 @@ class DBTable(db_api.DBTable):
             for row in rows:
                 flag = True
                 for c in criteria:
-                    if not calculateOp(type(c.value)(row[self.fields.index(db_api.DBField(c.field_name, type(c.value)))]), c.value, c.operator):
+                    if not calculateOp(
+                            type(c.value)(row[self.fields.index(db_api.DBField(c.field_name, type(c.value)))]), c.value,
+                            c.operator):
                         flag = False
 
                 if flag:
@@ -193,7 +205,9 @@ class DataBase(db_api.DataBase):
                 self.tables[tableName].counterRecords = len(read) - 1
                 self.tables[tableName].files = [file]
                 self.numOfTables += 1
-
+            # rows = readFromFile(file)[1:]
+            # for i, row in enumerate(rows):
+            #   self.tables[tableName].index[self.tables[tableName].key_field_name][row[0]] = (file, i + 1)
 
     # Put here any instance information needed to support the API
     def create_table(self, table_name: str, fields: List[db_api.DBField], key_field_name: str) -> DBTable:
@@ -218,14 +232,32 @@ class DataBase(db_api.DataBase):
     def get_tables_names(self) -> List[Any]:
         return list(self.tables.keys())
 
-'''
     def query_multiple_tables(
             self,
             tables: List[str],
-            fields_and_values_list: List[List[SelectionCriteria]],
+            fields_and_values_list: List[List[db_api.SelectionCriteria]],
             fields_to_join_by: List[str]
     ) -> List[Dict[str, Any]]:
-        raise NotImplementedError'''
+        final_res = []
+        match_records = []
+        for index_table in range(len(tables)):
+            match_records.append(self.get_table(tables[index_table]).query_table(fields_and_values_list[index_table]))
+        if len(match_records) < 2:
+            return match_records[0]
+        for i in range(1, len(match_records)):
+            final_res = []
+            for record1 in match_records[i - 1]:
+                for record2 in match_records[i]:
+                    for field in fields_to_join_by:
+                        if record1.get(field) != record2.get(field):
+                            break
+                    else:
+                        record1.update(record2)
+                        final_res.append(record1)
+            match_records[i] = final_res
+        return final_res
+
+        '''
 
 
 field1 = db_api.DBField("name", str)
@@ -248,10 +280,101 @@ for i in table.index:
     print(list(table.index[i].values()))
 
 print("***************")
-print(table.query_table([db_api.SelectionCriteria("age", "=", 4)]))
+print(table.query_table([db_api.SelectionCriteria("age", "=", 5)]))
 print(table.search(315437145))
 print(table.get_record(315437145))
 print(table.get_record(211426754))
+# table.delete_record(315437145)
 table.update_record(211426754, {'age': 21})
-print(table.get_record(211426754))
+table.update_record(315437145, {"name": "Malka Pnina"})
+'''
 
+
+'''
+
+field1 = db_api.DBField("name", str)
+field2 = db_api.DBField("age", int)
+field3 = db_api.DBField("id", int)
+table = DBTable()
+table.createNew("Person", [field3, field1, field2], "id")
+table.insert_record({"name": "Pnini", "age": 5, "id": 315437145})
+table.insert_record({"name": "Shira", "age": 4, "id": 211426754})
+table.create_index("age")
+table.insert_record({"name": "Elisheva", "age": 11, "id": 333094951})
+for i in table.index:
+    print(i)
+    print([(j, table.index[i][j]) for j in table.index[i]])
+table.delete_record(333094951)
+for i in table.index:
+    print(i)
+    print([(j, table.index[i][j]) for j in table.index[i]])
+    '''
+'''
+my_db = DataBase()
+fields_animals = [db_api.DBField("aid", str), db_api.DBField("aname", str), db_api.DBField("age", int),
+                  db_api.DBField("species", str)]
+fields_employees = [db_api.DBField("pid", str), db_api.DBField("pname", str), db_api.DBField("contact", int),
+                    db_api.DBField("species", str), db_api.DBField("age", int)]
+animals = my_db.create_table("animals", fields_animals, "aid")
+employees = my_db.create_table("employees", fields_employees, "pid")
+a1_dict = {"aid": "a1",
+           "aname": "aaa",
+           "age": 5,
+           "species": "lion"}
+a2_dict = {"aid": "a2",
+           "aname": "bbb",
+           "age": 7,
+           "species": "tiger"}
+a3_dict = {"aid": "a3",
+           "aname": "ddd",
+           "age": 7,
+           "species": "lion"}
+p1_dict = {"pid": "p1",
+           "pname": "Bob",
+           "contact": 123456,
+           "species": "lion",
+           "age": 7}
+p2_dict = {"pid": "p2",
+           "pname": "Dan",
+           "contact": 55555,
+           "species": "lion",
+           "age": 5}
+p3_dict = {"pid": "p3",
+           "pname": "Danniel",
+           "contact": 987654,
+           "species": "tiger",
+           "age": 7}
+animals.insert_record(a1_dict)
+# print(animals.count())
+animals.insert_record(a2_dict)
+# print(animals.count())
+animals.create_index("age")
+# animals.create_index("species")
+animals.insert_record(a3_dict)
+# print(animals.count())
+# animals.delete_record("a1")
+# print(animals.get_record("a2"))
+# a2_dict = {"aid": "a2",
+# "aname": "bbb",
+# "age": 7,
+# "species": "dog"}
+# animals.update_record("a2", a2_dict)
+employees.insert_record(p1_dict)
+employees.insert_record(p2_dict)
+employees.insert_record(p3_dict)
+# print(my_db.num_tables())
+# print("table names:")
+# print(my_db.get_tables_names())
+# my_db.delete_table("employees")
+# print("after delete:")
+# print(my_db.num_tables())
+c1 = db_api.SelectionCriteria("age", '>=', 5)
+c2 = db_api.SelectionCriteria("species", '=', "lion")
+# print(animals.query_table([c1]))
+# animals.delete_records([c1])
+# print(animals.count())
+c3 = db_api.SelectionCriteria("contact", '>=', 1)
+c4 = db_api.SelectionCriteria("age", '=', 5)
+print(my_db.query_multiple_tables(["employees", "animals"], [[c3], [c4]], ["species"]))
+# print(my_db.query_multiple_tables(["employees", "animals"],[c3, c4] ,["species", "age"]))
+'''
